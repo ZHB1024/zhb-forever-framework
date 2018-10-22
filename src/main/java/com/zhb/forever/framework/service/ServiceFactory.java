2b7bf0ccc266fffb76db0cb3875f249d2e8c1adc
ZHB-JOB <zhb20111503@126.com>
2018-10-22 14:14:11 +0800

    WebApplicationContext getBean

---
+++ src/main/java/com/zhb/forever/framework/service/ServiceFactory.java
@@ -0,0 +1,13 @@
+package com.zhb.forever.framework.service;
+
+/**
+*@author   zhanghb<a href="mailto:zhb20111503@126.com">zhanghb</a>
+*@createDate 2018年10月22日下午2:12:37
+*/
+
+public class ServiceFactory {
+
+
+}
+
+
