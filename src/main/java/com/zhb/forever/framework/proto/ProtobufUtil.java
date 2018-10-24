e9b13f58db1ff9751ab5884d5c414f447866dd09
ZHB-JOB <zhb20111503@126.com>
2018-10-24 13:50:15 +0800

    keyvalueprotobuf classpath

---
+++ src/main/java/com/zhb/forever/framework/proto/ProtobufUtil.java
@@ -0,0 +1,14 @@
+package com.zhb.forever.framework.proto;
+
+/**
+*@author   zhanghb<a href="mailto:zhb20111503@126.com">zhanghb</a>
+*@createDate 2018年10月24日下午1:46:28
+*/
+
+public class ProtobufUtil {
+
+    public static final String KEY_VALUE_PROTOBUF_CLASS_PATH = "com.zhb.forever.framework.proto.protobuf.KeyValueProtobuf$KeyValue";
+
+}
+
+
