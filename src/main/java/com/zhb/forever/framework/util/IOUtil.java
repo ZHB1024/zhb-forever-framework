d0a2719b77dff988872b2a581b12e4b9b871151f
ZHB-JOB <zhb20111503@126.com>
2018-11-15 16:58:34 +0800

    mail

--- src/main/java/com/zhb/forever/framework/util/IOUtil.java
+++ src/main/java/com/zhb/forever/framework/util/IOUtil.java
@@ -1,8 +1,9 @@
 package com.zhb.forever.framework.util;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import org.apache.commons.io.IOUtils;
 
 /**
 *@author   zhanghb<a href="mailto:zhb20111503@126.com">zhanghb</a>
@@ -12,21 +13,13 @@ import java.io.OutputStream;
 public class IOUtil {
 
     public static void closeQuietly(InputStream input) {
-        try {
-            if (input != null)
-                input.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        if (input != null)
+            IOUtils.closeQuietly(input);
     }
 
     public static void closeQuietly(OutputStream output) {
-        try {
-            if (output != null)
-                output.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        if (output != null)
+            IOUtils.closeQuietly(output);
     }
 
 }
