1ac00e3ebfc3576b1982e2a77d5e549c1855a783
ZHB-JOB <zhb20111503@126.com>
2018-12-12 18:04:38 +0800

    cpu 数量

--- src/main/java/com/zhb/forever/framework/util/CPUUtil.java
+++ src/main/java/com/zhb/forever/framework/util/CPUUtil.java
@@ -1,13 +1,25 @@
 package com.zhb.forever.framework.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
 *@author   zhanghb<a href="mailto:zhb20111503@126.com">zhanghb</a>
 *@createDate 2018年10月31日上午9:08:43
 */
 
 public class CPUUtil {
+    
+    private static Logger logger = LoggerFactory.getLogger(CPUUtil.class);
 
     public static final int NCPU = Runtime.getRuntime().availableProcessors();
+    
+    
+    public static void main(String[] args) {
+        
+        System.out.println(NCPU);
+        
+    }
 
 }
 
