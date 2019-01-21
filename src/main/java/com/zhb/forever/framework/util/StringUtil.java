0ef458d00f918a87e721475555f0b3443cfc7883
ZHB-JOB <zhb20111503@126.com>
2019-01-21 17:55:31 +0800

    string 参数拼接

--- src/main/java/com/zhb/forever/framework/util/StringUtil.java
+++ src/main/java/com/zhb/forever/framework/util/StringUtil.java
@@ -735,5 +735,57 @@ public class StringUtil {
     public static String replaceLast( String text, String oldChar, String newChar ) {
         return text.replaceFirst( "(?s)" + oldChar + "(?!.*?" + oldChar + ")", newChar );
     }
+    
+    /**
+     * solr 查询参数拼接
+     * @param field
+     * @param list
+     * @return BatchUploadExcelVO
+     */
+    public static String generateFilterOrQuery(String field, List<String> list) {
+        return generateFilterOrQuery(field, list.toArray());
+    }
+    
+    public static String generateFilterOrQuery(String field, Object[] strs) {
+        String format = "%s:(%s)";
+        return String.format(format, new Object[] { field, join(strs, " OR ") });
+    }
+
+    public static String join(Object[] array, String separator) {
+        if (array == null) {
+            return null;
+        }
+        return join(array, separator, 0, array.length);
+    }
+
+    public static String join(Object[] array, String separator, int startIndex, int endIndex) {
+        if (array == null) {
+            return null;
+        }
+        if (separator == null) {
+            separator = "";
+        }
+
+        int bufSize = endIndex - startIndex;
+        if (bufSize <= 0) {
+            return "";
+        }
+
+        bufSize *= (((array[startIndex] == null) ? 16 : array[startIndex].toString().length()) + separator.length());
+
+        StringBuffer buf = new StringBuffer(bufSize);
+
+        for (int i = startIndex; i < endIndex; ++i) {
+            if (i > startIndex) {
+                buf.append(separator);
+            }
+            if (array[i] != null) {
+                buf.append("\"");
+                buf.append(array[i]);
+                buf.append("\"");
+            }
+        }
+        return buf.toString();
+    }
 
 }
